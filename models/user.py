a6effbb611582ba6365a632557500c1fd985e02e
Kolton Rhodes <gamerlife467@gmail.com>
2023-07-05 20:47:43 -0500

    updatemodels

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class State(BaseModel, Base):
+    """ State class """
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            return [
+                city for city in list(
+                    storage.all(City).values()) if city.state_id == self.id]
