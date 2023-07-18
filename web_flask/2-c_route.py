e0372c31c3f96e39df94a42b6ff2b5553886081a
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 <5574@holbertonstudents.com>
2023-07-17 22:20:56 -0500

    adding lines

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,10 +13,12 @@ def hello_route():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
     """Displays 'HBNB'"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
