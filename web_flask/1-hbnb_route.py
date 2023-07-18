33d4f3eff691d8eb3b055fca24a249d589c719d6
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 <5574@holbertonstudents.com>
2023-07-17 22:03:36 -0500

    adding corrected file

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,9 +14,9 @@ def hello_route():
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_route():
+def hbnb_route():
     """Displays 'HBNB'"""
     return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
